f69a6a899f4a10178d36e0e69b6282a5fe42bf1a
Begzod <ebegzod477@gmail.com>
2022-06-30 21:39:15 +0500

    code08

---
+++ bin/basic_func08.dart
@@ -0,0 +1,12 @@
+//  Create a function called func.
+
+//  Create function arguments a.
+
+//  Increase the value of a to one and return.
+int func(a){
+  return a+1;  
+}
+void main() {
+  
+    print(func(7));
+}
