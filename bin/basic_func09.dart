835653bf9fa35a77f3426321778b606d4415589a
Begzod <ebegzod477@gmail.com>
2022-06-30 21:42:19 +0500

    code09

---
+++ bin/basic_func09.dart
@@ -0,0 +1,12 @@
+//  Create a function called func.
+
+//  Create function arguments a.
+
+//  decrease the value of a to one and return
+int func(a){
+  return a-1;  
+}
+void main() {
+  
+    print(func(8));
+}
