1c18c8d660c08c43a2e7abc68799e1a4281881ea
Begzod <ebegzod477@gmail.com>
2022-06-30 21:44:56 +0500

    code10

---
+++ bin/basic_func10.dart
@@ -0,0 +1,12 @@
+//  Create a function called func.
+
+//  Create function arguments a.
+
+//  Return the negative value of a.
+int func(a){
+  return -a;  
+}
+void main() {
+  
+    print(func(7));
+}
